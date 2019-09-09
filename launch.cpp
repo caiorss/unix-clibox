f93d7776a7dd0b147a373c40aa609e818b20971d
CaioRS <caiorss.rodrigues@gmail.com>
2019-09-09 00:55:36 -0300

    Create class AppLauncher and implement log file redirecting.

--- launch.cpp
+++ launch.cpp
@@ -20,7 +20,8 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <signal.h>
-
+#include <sys/stat.h>
+#include <fcntl.h>
 
 bool is_tty_terminal()
 {
@@ -31,65 +32,104 @@ bool is_tty_terminal()
     #endif
 }
 
-/** @brief Launch executable as daemon process.
-  *
-  * @details It launches some application as a daemon process, without killing current
-  *          process or littering the current terminal with stdout and stderr output
-  *          of the launched process.
-  *
-  * @param program Program to be launched
-  * @param args    Arguments of the program to be launched
-  * @param cwd     Current working directory of the program to be launched
-  *
-  **********************************************************************************/
-std::optional<int>
-launch_as_daemon( std::string              const& program
-                , std::vector<std::string> const& args
-                , std::string              const& cwd = ".")
-{
-    int pid = ::fork();
 
-    // If the PID of the forked process is negative
-    // when the fork operation has fails.
-    if(pid < 0) {
-        std::cerr << " [ERROR] Unable to fork proces and launch " << program << "\n";
-        return std::nullopt;
+class AppLauncher
+{
+    std::string                m_program;
+    std::string                m_cwd      = ".";
+    std::optional<std::string> m_logfile  = std::nullopt;
+    bool                       m_terminal = false;
+
+public:
+
+    AppLauncher(std::string program)
+        : m_program(std::move(program))
+    { }
+
+    void set_cwd(std::string cwd)
+    {
+        this->m_cwd = std::move(cwd);
     }
 
-    // If the PID is greater than zero, the current process
-    // is the original or the parent one. Then the PID is returned
-    // to the caller.
-    if(pid > 0) { return pid; }
+    void set_logfile(std::string logfile)
+    {
+        this->m_logfile = std::move(logfile);
+    }
 
-    // ---- Forked process (pid == 0) --- //
-    //------------------------------------//
-    assert(pid == 0 && "PID should be zero");
-    ::setsid();
-    ::umask(0);
+    void set_terminal(bool flag)
+    {
+        this->m_terminal = flag;
+    }
 
-    // Set current directory of daemon process
-    ::chdir(cwd.c_str());
+    std::optional<int>
+    launch(std::vector<std::string> const& args = {})
+    {
+        if(!m_terminal)
+            return this->launch_impl(m_program, args);
+        else
+            return this->launch_impl("xterm", {"-hold", "-e", m_program});
+    }
 
-    // Close stdin, stdout and stderr file descriptors to avoid
-    // littering the parent process output.
-    ::close(STDIN_FILENO);
-    ::close(STDOUT_FILENO);
-    ::close(STDERR_FILENO);
+private:
 
-    std::vector<const char*> pargs(args.size() + 1);
-    pargs[0] = program.c_str();
-    std::transform(args.begin(), args.end(), pargs.begin() + 1
-                   , [](auto const& s){ return s.c_str(); });
-    pargs.push_back(nullptr);
+    std::optional<int>
+    launch_impl(std::string program, std::vector<std::string> const& args)
+    {
+        int pid = ::fork();
 
-    // Dummy return
-    return ::execvp(program.c_str(), (char* const*) &pargs[0]);
-}
+        // If the PID of the forked process is negative
+        // when the fork operation has fails.
+        if(pid < 0) {
+            std::cerr << " [ERROR] Unable to fork proces and launch " << program << "\n";
+            return std::nullopt;
+        }
 
-void launch_app_terminal(std::string const& application, std::string const& cwd = ".")
-{
-    launch_as_daemon("xterm", {"-hold", "-e", application }, cwd);
-}
+        // If the PID is greater than zero, the current process
+        // is the original or the parent one. Then the PID is returned
+        // to the caller.
+        if(pid > 0) { return pid; }
+
+        // ---- Forked process (pid == 0) --- //
+        //------------------------------------//
+        assert(pid == 0 && "PID should be zero");
+        ::setsid();
+        ::umask(0);
+
+        // Set current directory of daemon process
+        ::chdir(m_cwd.c_str());
+
+        // Close stdin, stdout and stderr file descriptors to avoid
+        // littering the parent process output.
+        ::close(STDIN_FILENO);
+        ::close(STDOUT_FILENO);
+        ::close(STDERR_FILENO);
+
+        if(this->m_logfile)
+        {
+            FILE* fp = ::fopen(m_logfile.value().c_str(), "w");
+            int fd = ::fileno(fp);
+            //int fd = open(logfile.value().c_str(), O_WRONLY);
+            if(fd == -1){
+                std::cerr << " [ERROR] Failed to open file: " << m_logfile.value() << "\n";
+            } else
+            {
+                ::dup2(fd, ::fileno(stdout));
+                ::dup2(fd, ::fileno(stderr));
+                ::dup2(fd, ::fileno(stdin));
+            }
+        }
+
+        std::vector<const char*> pargs(args.size() + 1);
+        pargs[0] = program.c_str();
+        std::transform(args.begin(), args.end(), pargs.begin() + 1
+                       , [](auto const& s){ return s.c_str(); });
+        pargs.push_back(nullptr);
+
+        // Dummy return
+        return ::execvp(program.c_str(), (char* const*) &pargs[0]);
+    }
+};
+
 
 /** Print directories listed in PATH environment variable.
  *  Operating Systems.
@@ -130,8 +170,9 @@ void relaunch_app_pid(int pid)
     // Kill process
     ::kill(pid, 9);
     // Restart process
-    auto pid_new = launch_as_daemon(exe.value(), {}, cwd.value());
-
+    AppLauncher app(exe.value());
+    app.set_cwd(cwd.value());
+    auto pid_new = app.launch();
     if(!pid_new){
         throw std::runtime_error("Error: failed to relaunch process");
     }
@@ -170,8 +211,13 @@ int main(int argc, char** argv)
                       , "Launch application in terminal");
 
     std::string cwd = ".";
-    cmd_run->add_option("-d,--dir", cwd
+    cmd_run->add_option("-d,--directory", cwd
                         , "Current directory of launched process");
+
+    std::string logfile = "";
+    cmd_run->add_option("--logfile", logfile
+                        , "Log file to which the process output will be redirected to.");
+
 
     //----- Path command settings -----------------//
 
@@ -205,15 +251,16 @@ int main(int argc, char** argv)
 
     // Command: run => Launch a process
     if(*cmd_run){
-        if(!flag_terminal)
-        {
-            auto pid = launch_as_daemon(application, {}, cwd);
-            if(pid) {
-                std::cout << " [INFO] Forked process launched successfully.\n"
-                          << " [INFO] Process pid = " << pid.value() << "\n";
-            }
-        } else {
-            launch_app_terminal(application, cwd);
+        // auto pid = launch_as_daemon(application, {}, cwd);
+        auto app = AppLauncher(application);
+        app.set_cwd(cwd);
+        app.set_terminal(flag_terminal);
+        if(logfile != "") app.set_logfile(logfile);
+        auto pid = app.launch({});
+
+        if(pid) {
+            std::cout << " [INFO] Forked process launched successfully.\n"
+                      << " [INFO] Process pid = " << pid.value() << "\n";
         }
         return EXIT_SUCCESS;
     }
